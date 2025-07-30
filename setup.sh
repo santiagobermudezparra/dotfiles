bf78c0870e552d3a6e51c67d41ae3765cb97b963
Santiago <santiago.bermudezparra@smartapps.co.nz>
2025-07-31 11:39:16 +1200

    dotfiles Mischa

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# Install chezmoi if missing
-if ! command -v chezmoi >/dev/null; then
-    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:santiagobermudezparra/dotfiles.git
-fi
-
-exit 0
