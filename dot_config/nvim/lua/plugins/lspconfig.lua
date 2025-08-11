45f6a319a9ca73320a2b9f1cc1903b4781858e7f
Santiago <santiago.bermudezparra@smartapps.co.nz>
2025-08-12 10:11:57 +1200

    autocompletion vim  v2

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -3,59 +3,16 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        -- Python
-        pyright = {
+        bashls = {},  -- Bash support
+        marksman = {  -- Markdown with custom settings
           settings = {
-            python = {
-              analysis = {
-                typeCheckingMode = "basic",
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
+            marksman = {
+              hover = {
+                openCommand = "xdg-open",
               },
             },
           },
         },
-        
-        -- Bash
-        bashls = {},
-        
-        -- Lua (for Neovim config)
-        lua_ls = {
-          settings = {
-            Lua = {
-              workspace = {
-                checkThirdParty = false,
-              },
-              completion = {
-                callSnippet = "Replace",
-              },
-              diagnostics = {
-                globals = { "vim" },
-              },
-            },
-          },
-        },
-        
-        -- JSON
-        jsonls = {},
-        
-        -- YAML
-        yamlls = {
-          settings = {
-            yaml = {
-              keyOrdering = false,
-            },
-          },
-        },
-        
-        -- Markdown
-        marksman = {},
-        
-        -- Docker
-        dockerls = {},
-        
-        -- TOML
-        taplo = {},
       },
     },
   },
